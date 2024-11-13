6843df23887a6e890f48dd1bc210e403efe90e5f
vietthuan03 <105033258+vietthuan03@users.noreply.github.com>
2024-11-13 23:02:48 +0700

    checkout valid

--- lib/presentation/pages/Checkout.dart
+++ lib/presentation/pages/Checkout.dart
@@ -1,29 +1,35 @@
-import 'package:car_servicing/responsive/ResponsiveWidget.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'package:car_servicing/presentation/pages/payment.dart';
+import 'package:car_servicing/presentation/widgets/date_time_custom.dart';
 import 'package:flutter/material.dart';
 
-
 class CheckoutScreen extends StatefulWidget {
+  const CheckoutScreen({super.key});
+
   @override
   _CheckoutScreenState createState() => _CheckoutScreenState();
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
-  List<String> availableDates = ['Today 21', 'Tomorrow 22', 'Wed 23', 'Thu 24'];
-  int selectedDateIndex = 0;
-  int selectedTimeSlotIndex = -1;
+  bool isDateTimeSelected = false;
 
-  List<String> timeSlots = [
-    '08:00 - 08:30', '09:00 - 09:30', '09:30 - 10:00', '10:00 - 10:30',
-    '10:30 - 11:00', '11:00 - 11:30', '13:30 - 14:00', '14:00 - 14:30',
-    '14:30 - 15:00', '15:00 - 15:30', '15:30 - 16:00', '16:30 - 17:00'
-  ];
+  void _onDateSelected(DateTime dateTime) {
+    final now = DateTime.now();
+    final bool isValidTime = dateTime.isAfter(now) &&
+        (dateTime.hour > 8 || (dateTime.hour == 8 && dateTime.minute >= 0)) &&
+        (dateTime.hour < 17 ||
+            (dateTime.hour == 17 &&
+                dateTime.minute == 0)); // Ensure time between 08:00 and 17:00
+
+    setState(() {
+      isDateTimeSelected = isValidTime;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Checkout'),
+        title: const Text('Checkout'),
         backgroundColor: Colors.blue,
       ),
       body: Padding(
@@ -31,128 +37,66 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Add Pick-up Address',
               style: TextStyle(
                 fontSize: 16,
                 color: Colors.black45,
               ),
             ),
-            SizedBox(height: 16),
-            Text(
+            const SizedBox(height: 16),
+            const Text(
               'When do you want the service?',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             Expanded(
-              // flex: ResponsiveWidget.isTablet(context)? 2:1,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Select Date',
+                  const Text(
+                    'Select Date & Time',
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                   ),
-                  SizedBox(height: 8),
-                  CarouselSlider(
-                    options: CarouselOptions(
-                      height: 50.0,
-                      enableInfiniteScroll: false,
-                      onPageChanged: (index, reason) {
-                        setState(() {
-                          selectedDateIndex = index;
-                        });
-                      },
-                    ),
-                    items: availableDates.map((date) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width / 4,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              color: selectedDateIndex ==
-                                      availableDates.indexOf(date)
-                                  ? Colors.blue[100]
-                                  : Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Colors.grey),
-                            ),
-                            child: Center(
-                              child: Text(
-                                date,
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'Select Pick-up Time Slot',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                  SizedBox(height: 8),
-                  Expanded(
-                    flex: ResponsiveWidget.isDesktop(context) ? 2 : 1,
-                    child: GridView.builder(
-                      shrinkWrap: true,
-                      physics: const BouncingScrollPhysics(),
-                      // Disable scrolling within the GridView
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 8.0,
-                        crossAxisSpacing: 8.0,
-                        childAspectRatio: 3,
-                      ),
-                      itemCount: timeSlots.length,
-                      itemBuilder: (context, index) {
-                        return ChoiceChip(
-                          label: Text(timeSlots[index]),
-                          selected: selectedTimeSlotIndex == index,
-                          onSelected: (bool selected) {
-                            setState(() {
-                              selectedTimeSlotIndex = selected ? index : -1;
-                            });
-                          },
-                          selectedColor: Colors.blue[100],
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        );
-                      },
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: DateTimePickerCustom(
+                      onDateSelected: _onDateSelected,
                     ),
                   ),
+                  const SizedBox(height: 16),
                 ],
               ),
             ),
-            Text(
+            const Text(
               'Basic Service 2.599.000 VND',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: selectedTimeSlotIndex != -1
+              onPressed: isDateTimeSelected
                   ? () {
-                      // Handle Proceed Action
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text('Proceeding with booking...'),
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PaymentPage(),
                         ),
                       );
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //   const SnackBar(
+                      //     content: Text('Proceed to the next screen...'),
+                      //   ),
+                      // );
                     }
                   : null,
-              child: Text('PROCEED'),
               style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 16),
-                minimumSize: Size(double.infinity, 50),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                minimumSize: const Size(double.infinity, 50),
                 backgroundColor: Colors.blue,
               ),
+              child: const Text('PROCEED'),
             ),
           ],
         ),
