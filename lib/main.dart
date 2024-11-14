6bf045cb0c25d84185a04a5cacef20e5917711f0
vietthuan03 <105033258+vietthuan03@users.noreply.github.com>
2024-11-14 21:44:57 +0700

    transition

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
-
 import 'package:car_servicing/firebase_option.dart';
 import 'package:car_servicing/presentation/pages/user/EditUserProfile.dart';
 import 'package:car_servicing/presentation/pages/auth/Login.dart';
 import 'package:car_servicing/presentation/pages/auth/Registation.dart';
-import 'package:car_servicing/presentation/pages/Settings.dart';
+import 'package:car_servicing/presentation/pages/settings.dart';
 import 'package:car_servicing/presentation/pages/user/UserProfile.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 
 Future<void> main() async {
   //firebase authentication
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+
   runApp(MyApp());
 }
 
@@ -27,15 +25,15 @@ class MyApp extends StatelessWidget {
     //   ),
     //   home: CheckoutScreen(),
     // );
-    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: Login.id,
-      routes: { 
+      routes: {
         UserProfile.id: (context) => const UserProfile(),
         EditUserProfile.id: (context) => const EditUserProfile(),
-        Settings.id:(context) => const Settings(),
-        Login.id:(context)=> const Login(),
+        Settings.id: (context) => const Settings(),
+        Login.id: (context) => const Login(),
         Registation.id: (context) => const Registation(),
       },
     );
